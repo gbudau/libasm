6c7a5361f6547fc8344031d9ee01617cbce6beb8
gbudau <gbudau@student.42.fr>
2020-08-12 00:03:29 +0200

    Added malloc protection in ft__strdup

--- ft__strdup.s
+++ ft__strdup.s
@@ -12,11 +12,16 @@ ft__strdup:
 		mov		rdi, rax			; copy length in rdi for malloc
 		inc		rdi					; increment rdi for null byte
 		call	malloc wrt ..plt	; allocate memory
+		test	rax, rax			; check if there was an error and rax is NULL
+		je		.error				; jump to .error in case rax is NULL
 		mov		rcx, rax			; save address of allocated space into rcx
 .copy:
 		mov		rdi, rax			; set dst to malloc return address
 		pop		rsi					; restore rdi into rsi
 		call	ft__strcpy			; copy string from src to dst
-.done
-		mov		rax, rcx			; set rax to address of allocated space
-		ret
+.done:
+		mov		rax, rcx			; set rax to address of allocated memory
+		ret							; return the address of allocated memory
+
+.error:
+		ret							; return in case of error, rax will be NULL
